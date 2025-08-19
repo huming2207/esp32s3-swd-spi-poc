2fc68aeb295bfeb81a93436ce82e61f50dafaad8
Jackson Ming Hu <huming2207@gmail.com>
2025-08-19 22:19:13 +1000

    main: also send two more turnaround after reset (or otherwise no ACK)

--- main/poc_main.cpp
+++ main/poc_main.cpp
@@ -8,10 +8,13 @@
 
 extern "C" void app_main(void)
 {
-    ESP_ERROR_CHECK(swd_spi_init(GPIO_NUM_1, GPIO_NUM_2, 800000, SPI2_HOST));
+    ESP_ERROR_CHECK(swd_spi_init(GPIO_NUM_1, GPIO_NUM_2, 1000000, SPI2_HOST));
     ESP_ERROR_CHECK(swd_spi_reset());
     ESP_ERROR_CHECK(swd_spi_switch());
     ESP_ERROR_CHECK(swd_spi_reset());
+    swd_spi_send_trn(2); // WTF??
+
+    vTaskDelay(10);
 
     uint32_t idcode = 0;
     if (swd_spi_read_idcode(&idcode) != ESP_OK) {
